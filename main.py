42e01f269747a3c058b964c695b0e3083390756d
mybell314 <my.bell@dameallans.co.uk>
2025-02-05 11:04:36 +0000

    strobe rectangle

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import os
 import random
 import pygame
 
+#player
+class Player(object):
+    def __init__(self):
+        self.rect = pygame.Rect(30, 30, 60, 60)
+
+    def move(self, dx, dy):
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+
+    def move_single_axis(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
 #start pygame
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
@@ -9,19 +24,30 @@ pygame.init()
 #set up display
 pygame.display.set_caption("Fortnite 2")
 screen = pygame.display.set_mode((420, 340))
-
+clock = pygame.time.Clock()
+player = Player()
 colour = (0, 128, 255)
 
 #main loop
 running = True
 
 while running:
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if (event.type == pygame.KEYDOWN) and (event.key == pygame.K_SPACE):
+            if colour == (0, 128, 255):
+                colour = (255, 100, 0)
+            else:
+                colour = (0, 128, 255)
+
 
     #draw screen
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, colour, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.rect(screen, colour, player.rect)
     pygame.display.flip()
+
 pygame.quit()
